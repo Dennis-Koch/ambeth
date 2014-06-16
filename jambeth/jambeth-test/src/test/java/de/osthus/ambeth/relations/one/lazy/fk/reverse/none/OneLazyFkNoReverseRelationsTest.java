47d4ffb0a595f92be47a7a33e0de5d185734d4ac
Jochen Hormes <jochen.hormes@osthus.de>
2014-06-16 13:39:50 +0200

    Fixed tests failing on Jenkins and added them to the AllAmbethTests suite.

--- jambeth/jambeth-test/src/test/java/de/osthus/ambeth/relations/one/lazy/fk/reverse/none/OneLazyFkNoReverseRelationsTest.java
+++ jambeth/jambeth-test/src/test/java/de/osthus/ambeth/relations/one/lazy/fk/reverse/none/OneLazyFkNoReverseRelationsTest.java
@@ -40,7 +40,7 @@ public class OneLazyFkNoReverseRelationsTest extends AbstractRelationsTest
 	{
 		EntityB entityB = cache.getObject(EntityB.class, 11);
 
-		EntityA entityA = new EntityA();
+		EntityA entityA = entityFactory.createEntity(EntityA.class);
 		entityA.setName("new EntityA");
 		entityB.setEntityA(entityA);
 		relationsService.save(entityB);
@@ -56,7 +56,7 @@ public class OneLazyFkNoReverseRelationsTest extends AbstractRelationsTest
 	{
 		EntityB entityB = cache.getObject(EntityB.class, 11);
 
-		EntityA entityA = new EntityA();
+		EntityA entityA = entityFactory.createEntity(EntityA.class);
 		entityA.setName("new EntityA");
 		relationsService.save(entityA);
 
