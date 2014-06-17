8e7d7a4447d9202a8af63ba72d065b2ff54ea3ca
Jochen Hormes <jochen.hormes@osthus.de>
2014-06-17 17:24:00 +0200

    osthus-ambeth: Incremented minor version to 2.1.

--- jambeth/jambeth-test/src/test/java/de/osthus/ambeth/relations/AllRelationTests.java
+++ jambeth/jambeth-test/src/test/java/de/osthus/ambeth/relations/AllRelationTests.java
@@ -7,8 +7,12 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ de.osthus.ambeth.relations.many.lazy.fk.forward.none.ManyLazyFkForwardNoneRelationsTest.class,
 		de.osthus.ambeth.relations.many.lazy.fk.reverse.none.ManyLazyFkNoReverseRelationsTest.class,
+		de.osthus.ambeth.relations.many.lazy.fk.reverse.none.ManyLazyNoReverseRelationsTest.class,
 		de.osthus.ambeth.relations.many.lazy.link.reverse.none.ManyLazyNoLinkReverseRelationsTest.class,
-		de.osthus.ambeth.relations.one.lazy.fk.reverse.none.OneLazyFkNoReverseRelationsTest.class })
+		de.osthus.ambeth.relations.one.lazy.fk.reverse.none.OneLazyFkNoReverseRelationsTest.class,
+		de.osthus.ambeth.relations.one.fk.reverse.none.OneEagerNoReverseRelationsTest.class,
+		de.osthus.ambeth.relations.one.fk.reverse.none.OneEagerVersionNoReverseRelationsTest.class,
+		de.osthus.ambeth.relations.one.fk.reverse.none.OneLazyNoReverseRelationsTest.class })
 public class AllRelationTests
 {
 
