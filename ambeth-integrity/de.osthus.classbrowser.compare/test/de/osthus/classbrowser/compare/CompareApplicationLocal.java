309b62a921b424cb78ad8586ab430743cd48f36a
Jochen Hormes <jochen.hormes@osthus.de>
2014-06-27 14:47:33 +0200

    ambeth-integrity: Path correction in test tool.

--- ambeth-integrity/de.osthus.classbrowser.compare/test/de/osthus/classbrowser/compare/CompareApplicationLocal.java
+++ ambeth-integrity/de.osthus.classbrowser.compare/test/de/osthus/classbrowser/compare/CompareApplicationLocal.java
@@ -10,7 +10,7 @@ public class CompareApplicationLocal
 		System.setProperty(CompareApplication.ARG_KEY_JAVAFILE, "C:/dev/osthus-extensions/data/" + OutputUtil.EXPORT_FILE_NAME);
 		System.setProperty(CompareApplication.ARG_KEY_CSHARPFILE, "C:/dev/osthus-extensions/data/export_csharp.xml");
 		System.setProperty(CompareApplication.ARG_KEY_RESULTTYPE, CompareApplication.EXPORT_TYPE_CHECKSTYLE);
-		System.setProperty(CompareApplication.ARG_KEY_TARGETPATH, "C:/dev/osthus-extensions/result");
+		System.setProperty(CompareApplication.ARG_KEY_TARGETPATH, "C:/dev/osthus-extensions/results");
 
 		CompareApplication.main(new String[0]);
 	}
