0e5daebc48070d480f50191f9ac918e5aa8d72b2
Jochen Hormes <jochen.hormes@osthus.de>
2014-07-02 15:04:35 +0200

    ambeth: Added workaround to force MSTest.exe to copy all dlls not only the directly referenced ones.

--- ambeth/Ambeth.Test/ambeth/testutil/AbstractInformationBusTest.cs
+++ ambeth/Ambeth.Test/ambeth/testutil/AbstractInformationBusTest.cs
@@ -2,9 +2,26 @@ using De.Osthus.Ambeth.Config;
 using De.Osthus.Ambeth.Ioc;
 using De.Osthus.Ambeth.Log;
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace De.Osthus.Ambeth.Testutil
 {
+    // Workaround to get MSTest.exe to copy all needed dlls and not only the directly referenced ones.
+    [DeploymentItem(@"Ambeth.Bytecode.dll")]
+    [DeploymentItem(@"Ambeth.Cache.dll")]
+    [DeploymentItem(@"Ambeth.Cache.Bytecode.dll")]
+    [DeploymentItem(@"Ambeth.Cache.DataChange.dll")]
+    //[DeploymentItem(@"Ambeth.Cache.Stream.dll")]
+    [DeploymentItem(@"Ambeth.DataChange.dll")]
+    [DeploymentItem(@"Ambeth.Event.dll")]
+    [DeploymentItem(@"Ambeth.Event.DataChange.dll")]
+    [DeploymentItem(@"Ambeth.Mapping.dll")]
+    [DeploymentItem(@"Ambeth.Merge.dll")]
+    [DeploymentItem(@"Ambeth.Merge.Bytecode.dll")]
+    [DeploymentItem(@"Ambeth.Security.dll")]
+    //[DeploymentItem(@"Ambeth.Sensor.dll")]
+    [DeploymentItem(@"Ambeth.Service.dll")]
+    //[DeploymentItem(@"Ambeth.Stream.dll")]
     [TestFrameworkModule(typeof(BytecodeModule), typeof(CacheModule), typeof(CacheBytecodeModule), typeof(CacheDataChangeModule), /*typeof(CacheStreamModule), */typeof(DataChangeModule),
 		    typeof(EventModule), typeof(EventDataChangeModule), typeof(MappingModule), typeof(MergeModule), typeof(CompositeIdModule), typeof(ObjectCopierModule), typeof(SecurityModule),
         /*typeof(SensorModule), */typeof(ServiceModule)/*, typeof(StreamModule)*/)]
