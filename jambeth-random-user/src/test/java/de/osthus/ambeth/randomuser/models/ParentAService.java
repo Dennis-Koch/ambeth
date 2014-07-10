6f7a2aadafbc1bfd6ec399e25ee73603a5922306
Jochen Hormes <jochen.hormes@osthus.de>
2014-07-10 15:28:14 +0200

    jambeth-random-user: Moved or copied files from normal jambeth projects.

---
+++ jambeth-random-user/src/test/java/de/osthus/ambeth/randomuser/models/ParentAService.java
@@ -0,0 +1,37 @@
+package de.osthus.ambeth.randomuser.models;
+
+import de.osthus.ambeth.log.ILogger;
+import de.osthus.ambeth.log.LogInstance;
+import de.osthus.ambeth.proxy.Service;
+
+@Service(IParentAService.class)
+public class ParentAService implements IParentAService
+{
+	@SuppressWarnings("unused")
+	@LogInstance(ParentAService.class)
+	private ILogger log;
+
+	@Override
+	public ParentA create(ParentA entity)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ParentA retrieve(int id)
+	{
+		return null;
+	}
+
+	@Override
+	public ParentA update(ParentA entity)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void delete(ParentA entity)
+	{
+		throw new UnsupportedOperationException();
+	}
+}
