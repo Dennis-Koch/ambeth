9818976288003365a5afe45af6ea969961632028
Jochen Hormes <jochen.hormes@osthus.de>
2014-07-15 08:45:42 +0200

    random-user: Refactoring of Maven goals.

--- random-user/random-user-maven-plugin/src/main/java/de/osthus/maven/randomuser/RemoveRandomUser.java
+++ random-user/random-user-maven-plugin/src/main/java/de/osthus/maven/randomuser/RemoveRandomUser.java
@@ -1,13 +1,6 @@
 package de.osthus.maven.randomuser;
 
-import java.io.File;
-
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-
-import de.osthus.ambeth.testutil.RandomUserScript;
 
 /**
  * Goal which removes an oracle db user with a random name.
@@ -16,35 +9,21 @@ import de.osthus.ambeth.testutil.RandomUserScript;
  * 
  */
 @Mojo(name = "remove", requiresDirectInvocation = true, aggregator = true)
-public class RemoveRandomUser extends AbstractMojo
+public class RemoveRandomUser extends AbstractRandomUserMojo
 {
-	private static final String CITEMP_PROPERTIES_NAME = "${basedir}/citemp.properties";
-
-	/**
-	 * Location of the citemp properties file.
-	 */
-	@Parameter(property = "citemp.file", defaultValue = CITEMP_PROPERTIES_NAME, required = true)
-	private File citempFile;
-
-	/**
-	 * Location of the test database properties file.
-	 */
-	@Parameter(property = "property.file", required = true)
-	private File propertyFile;
-
 	@Override
-	public void execute() throws MojoExecutionException
+	protected String[] getArgsArray()
 	{
 		String[] args = { "script.create=false", //
 				"script.user.propertyfile=" + citempFile, //
 				"property.file=" + propertyFile };
-		try
-		{
-			RandomUserScript.main(args);
-		}
-		catch (Throwable e)
-		{
-			throw new MojoExecutionException("Error during Oracle DB temp user removal", e);
-		}
+
+		return args;
+	}
+
+	@Override
+	protected String getErrorMessage()
+	{
+		return "Error during Oracle DB temp user creation";
 	}
 }
