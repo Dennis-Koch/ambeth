c15a37b33fb8b8bbc160f940ee8fd3763e60710f
Jochen Hormes <jochen.hormes@osthus.de>
2014-07-23 10:06:45 +0200

    project-hdf5: Moved reusable code to filesystem-common project.

--- osthus/project-hdf5/directory-filesystem/src/test/java/de/osthus/filesystem/directory/DirectoryFileSystemProviderTest.java
+++ osthus/project-hdf5/directory-filesystem/src/test/java/de/osthus/filesystem/directory/DirectoryFileSystemProviderTest.java
@@ -24,36 +24,18 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class DirectoryFileSystemProviderTest
 {
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception
-	{
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception
-	{
-	}
-
 	private DirectoryFileSystemProvider directoryFileSystemProvider;
 
 	@Before
 	public void setUp() throws Exception
 	{
 		directoryFileSystemProvider = new DirectoryFileSystemProvider();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
 	}
 
 	@Test
