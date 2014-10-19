883799cc937969c6cee087e2612ae5b39c3e80d5
Dennis Koch <dennis.koch@osthus.com>
2014-10-19 14:37:33 +0200

    doc: updated OfficeToImages

--- doc/OfficeToImages/Program.cs
+++ doc/OfficeToImages/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace OfficeToImages
@@ -16,20 +17,20 @@ namespace OfficeToImages
 
         static void Main(String[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 2 && args.Length != 1)
             {
-                Console.Error.WriteLine("Usage: OfficeToImages.exe <source-dir(s)> <target-dir>");
+                Console.Error.WriteLine("Usage: OfficeToImages.exe <source-dir(s)> [<target-dir>]");
                 Console.Error.WriteLine("source-dir(s): Semicolon (;) separated list of directories to scan for office documents");
-                Console.Error.WriteLine("target-dir: Directory where to store the generated images");
+                Console.Error.WriteLine("target-dir: Directory where to store the generated images. If omitted the current directory will be used");
                 return;
             }
             String sourceArg = args[0];
 
-            String target = args[1];
-
+            String target = args.Length > 1 ? args[1] : ".";
+            target = new DirectoryInfo(target).FullName;
+            
             Console.WriteLine("Saving all generated images to '" + target + "'");
-
-
+            
             try
             {
                 String[] sources = sourceArg.Split(';');
@@ -52,7 +53,7 @@ namespace OfficeToImages
 
                         if (name.EndsWith("ppt") || name.EndsWith("pptx"))
                         {
-                            Console.Write("Handling '" + name + "'...");
+                            Console.WriteLine("Handling '" + name + "'...");
                             if (ParsePowerPoint(file, name, target))
                             {
                                 Console.WriteLine("Done");
@@ -64,7 +65,7 @@ namespace OfficeToImages
                         }
                         else if (name.EndsWith("vsd"))
                         {
-                            Console.Write("Handling '" + name + "'...");
+                            Console.WriteLine("Handling '" + name + "'...");
                             if (ParseVisio(file, name, target))
                             {
                                 Console.WriteLine("Done");
@@ -108,6 +109,11 @@ namespace OfficeToImages
             return targetDir + "/" + sourceFileName + "-" + index + ".png";
         }
 
+        protected static String BuildTargetTempFileName(String sourceFileName, String targetDir, int index)
+        {
+            return targetDir + "/" + sourceFileName + "-" + index + "-temp.png";
+        }
+
         protected static bool ParsePowerPoint(String file, String name, String targetDir)
         {
             int index = 1;
@@ -126,8 +132,10 @@ namespace OfficeToImages
             {
                 foreach (Microsoft.Office.Interop.PowerPoint.Slide slide in ppt.Slides)
                 {
-                    String targetFile = BuildTargetFileName(name, targetDir, index);
-                    slide.Export(targetFile, "png", 1024, 768);
+                    DoExport(name, targetDir, index, delegate(String fileName)
+                    {
+                        slide.Export(fileName, "png", 1024, 768);
+                    });
                     index++;
                 }
                 return true;
@@ -157,8 +165,10 @@ namespace OfficeToImages
             {
                 foreach (Microsoft.Office.Interop.Visio.Page page in vsd.Pages)
                 {
-                    String targetFile = BuildTargetFileName(name, targetDir, index);
-                    page.Export(targetFile);
+                    DoExport(name, targetDir, index, delegate(String fileName)
+                    {
+                        page.Export(fileName);
+                    });
                     index++;
                 }
                 return true;
@@ -167,6 +177,62 @@ namespace OfficeToImages
             {
                 vsd.Saved = true;
                 vsd.Close();
+            }
+        }
+
+        protected static bool DoExport(String name, String targetDir, int index, ExportDelegate exportDelegate)
+        {
+            String targetFile = BuildTargetFileName(name, targetDir, index);
+            if (!File.Exists(targetFile))
+            {
+                exportDelegate(targetFile);
+                Console.WriteLine("\tCreated '" + targetFile + "'");
+                return true;
+            }
+            String tempTargetFile = BuildTargetFileName(name, targetDir, index);
+            exportDelegate(tempTargetFile);
+
+            if (AreFilesEqual(tempTargetFile, targetFile))
+            {
+                Console.WriteLine("\tSkipped '" + targetFile + "' (still current)");
+                return false;
+            }
+            File.Copy(tempTargetFile, targetFile, true);
+            Console.WriteLine("\tUpdated '" + targetFile + "'");
+            return true;
+        }
+
+        protected static bool AreFilesEqual(String leftFile, String rightFile)
+        {
+            // first check size
+            if (new FileInfo(leftFile).Length != new FileInfo(rightFile).Length)
+            {
+                return false;
+            }
+            // then check checksum
+            byte[] newChecksum = Checksum(leftFile);
+            byte[] oldChecksum = Checksum(rightFile);
+
+            if (newChecksum.Length != oldChecksum.Length)
+            {
+                return false;
+            }
+            for (int a = newChecksum.Length; a-- > 0; )
+            {
+                if (newChecksum[a] != oldChecksum[a])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        protected static byte[] Checksum(String file)
+        {
+            using (FileStream stream = File.OpenRead(file))
+            {
+                HashAlgorithm sha = new SHA256Managed();
+                return sha.ComputeHash(stream);
             }
         }
     }
