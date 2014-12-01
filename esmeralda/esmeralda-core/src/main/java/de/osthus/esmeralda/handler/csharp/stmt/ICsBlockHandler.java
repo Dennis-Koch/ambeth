302d858d94091f776865636a89eda27223b48471
Jochen Hormes <jochen.hormes@osthus.de>
2014-12-01 10:20:23 +0100

    esmeralda: Formatting.

--- esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/handler/csharp/stmt/ICsBlockHandler.java
+++ esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/handler/csharp/stmt/ICsBlockHandler.java
@@ -2,12 +2,7 @@ package de.osthus.esmeralda.handler.csharp.stmt;
 
 import com.sun.source.tree.BlockTree;
 
-import de.osthus.ambeth.log.ILogger;
-import de.osthus.ambeth.log.LogInstance;
-
 public interface ICsBlockHandler
 {
-
 	void writeBlockContentWithoutIntendation(BlockTree blockTree);
-
 }
