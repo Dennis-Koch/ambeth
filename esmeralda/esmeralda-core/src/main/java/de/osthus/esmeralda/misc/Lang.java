cae86204cfd140a6ae2508c6fe40e4537d58afc0
Jochen Hormes <jochen.hormes@osthus.de>
2014-12-23 12:26:33 +0100

    esmeralda: Added the last two missing statement handlers for JS. Refactored the expression handler registration.

--- esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/misc/Lang.java
+++ esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/misc/Lang.java
@@ -2,8 +2,6 @@ package de.osthus.esmeralda.misc;
 
 public final class Lang
 {
-	public static final String ALL = "all";
-
 	public static final String C_SHARP = "C#";
 
 	public static final String JS = "JS";
