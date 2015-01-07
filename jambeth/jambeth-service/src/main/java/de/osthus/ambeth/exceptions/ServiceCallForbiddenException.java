57c18d07d5e4a9838b8e7da98f292aba47cf9cef
Dennis Koch <dennis.koch@osthus.com>
2015-01-07 16:17:31 +0100

    jambeth: changed inheritance of ServiceCallForbiddenException

--- jambeth/jambeth-service/src/main/java/de/osthus/ambeth/exceptions/ServiceCallForbiddenException.java
+++ jambeth/jambeth-service/src/main/java/de/osthus/ambeth/exceptions/ServiceCallForbiddenException.java
@@ -1,7 +1,6 @@
 package de.osthus.ambeth.exceptions;
 
-
-public class ServiceCallForbiddenException extends RuntimeException
+public class ServiceCallForbiddenException extends SecurityException
 {
 	private static final long serialVersionUID = -5303216306654899560L;
 
