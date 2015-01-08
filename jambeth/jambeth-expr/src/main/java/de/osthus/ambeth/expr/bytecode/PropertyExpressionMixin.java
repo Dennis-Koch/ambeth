9facad9cda4d37687b91fc7c02810efccbbbf015
Dennis Koch <dennis.koch@osthus.com>
2015-01-08 14:06:39 +0100

    jambeth-expr-test: fixed cyclic maven dependency

--- jambeth/jambeth-expr/src/main/java/de/osthus/ambeth/expr/bytecode/PropertyExpressionMixin.java
+++ jambeth/jambeth-expr/src/main/java/de/osthus/ambeth/expr/bytecode/PropertyExpressionMixin.java
@@ -2,14 +2,13 @@ package de.osthus.ambeth.expr.bytecode;
 
 import de.osthus.ambeth.expr.IEntityPropertyExpressionResolver;
 import de.osthus.ambeth.expr.PropertyExpression;
-import de.osthus.ambeth.ioc.IInitializingBean;
 import de.osthus.ambeth.ioc.annotation.Autowired;
 import de.osthus.ambeth.log.ILogger;
 import de.osthus.ambeth.log.LogInstance;
 import de.osthus.ambeth.proxy.IEntityMetaDataHolder;
 import de.osthus.ambeth.util.IConversionHelper;
 
-public class PropertyExpressionMixin implements IInitializingBean
+public class PropertyExpressionMixin
 {
 	@SuppressWarnings("unused")
 	@LogInstance
@@ -21,20 +20,18 @@ public class PropertyExpressionMixin implements IInitializingBean
 	@Autowired(optional = true)
 	protected IEntityPropertyExpressionResolver entityPropertyExpressionResolver;
 
-	@Override
-	public void afterPropertiesSet() throws Throwable
-	{
-		if (entityPropertyExpressionResolver == null)
-		{
-			log.warn("INACTIVE: Annotation feature @" + PropertyExpression.class.getName() + " on entities. Reason: No instance of "
-					+ IEntityPropertyExpressionResolver.class.getName() + " resolved");
-		}
-	}
+	protected boolean firstUsage true;
 
 	public Object evaluate(IEntityMetaDataHolder entity, String expression, Class<?> expectedType)
 	{
 		if (entityPropertyExpressionResolver == null)
 		{
+			if (firstUsage)
+			{
+				firstUsage = false;
+				log.warn("INACTIVE: Annotation feature @" + PropertyExpression.class.getName() + " on entities. Reason: No instance of "
+						+ IEntityPropertyExpressionResolver.class.getName() + " resolved");
+			}
 			return conversionHelper.convertValueToType(expectedType, Integer.valueOf(0));
 		}
 		Object expressionResult = entityPropertyExpressionResolver.resolveExpressionOnEntity(entity, expression);
