be83728bb3c02b716f9f4ccd0a41c259135dd208
Dennis Koch <dennis.koch@osthus.com>
2015-01-13 17:19:47 +0100

    jambeth-log: added PropertyResolveException to src

---
+++ jambeth/jambeth-log/src/main/java/de/osthus/ambeth/exceptions/PropertyResolveException.java
@@ -0,0 +1,32 @@
+package de.osthus.ambeth.exceptions;
+
+public class PropertyResolveException extends RuntimeException
+{
+	private static final long serialVersionUID = -7149333583589679782L;
+
+	public PropertyResolveException()
+	{
+		super();
+	}
+
+	protected PropertyResolveException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public PropertyResolveException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public PropertyResolveException(String message)
+	{
+		super(message);
+	}
+
+	public PropertyResolveException(Throwable cause)
+	{
+		super(cause);
+	}
+
+}
