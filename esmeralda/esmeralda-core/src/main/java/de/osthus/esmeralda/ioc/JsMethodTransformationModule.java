fb2e713beaff65581995584f203f5c777fb18a5b
Jochen Hormes <jochen.hormes@osthus.de>
2015-01-27 10:41:20 +0100

    esmeralda: Made DefaultMethodParameterProcessor and DefaultMethodTransformer unique per language.

--- esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/ioc/JsMethodTransformationModule.java
+++ esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/ioc/JsMethodTransformationModule.java
@@ -5,26 +5,44 @@ import de.osthus.ambeth.ioc.config.IBeanConfiguration;
 import de.osthus.ambeth.ioc.factory.IBeanContextFactory;
 import de.osthus.esmeralda.handler.IMethodTransformerExtension;
 import de.osthus.esmeralda.handler.IMethodTransformerExtensionExtendable;
+import de.osthus.esmeralda.handler.js.transformer.DefaultMethodParameterProcessor;
+import de.osthus.esmeralda.handler.js.transformer.DefaultMethodTransformer;
 import de.osthus.esmeralda.handler.js.transformer.JavaIoPrintstreamTransformer;
 import de.osthus.esmeralda.handler.js.transformer.JavaLangCharSequenceTransformer;
 import de.osthus.esmeralda.handler.js.transformer.JavaLangStringTransformer;
+import de.osthus.esmeralda.handler.uni.transformer.AbstractMethodTransformerExtension;
 import de.osthus.esmeralda.misc.Lang;
 
 public class JsMethodTransformationModule implements IInitializingModule
 {
+	private static final String JsDefaultMethodTransformerName = Lang.JS + EsmeraldaCoreModule.DefaultMethodTransformerName;
+
+	private static final String JsDefaultMethodParameterProcessor = EsmeraldaCoreModule.DefaultMethodParameterProcessor + Lang.JS;
+
+	private IBeanConfiguration defaultMethodParameterProcessor;
+
+	private IBeanConfiguration defaultMethodTransformer;
+
 	@Override
 	public void afterPropertiesSet(IBeanContextFactory beanContextFactory) throws Throwable
 	{
+		defaultMethodParameterProcessor = beanContextFactory.registerBean(JsDefaultMethodParameterProcessor, DefaultMethodParameterProcessor.class);
+
+		defaultMethodTransformer = beanContextFactory.registerBean(DefaultMethodTransformer.class) //
+				.propertyRef(AbstractMethodTransformerExtension.defaultMethodParameterProcessorProp, defaultMethodParameterProcessor);
+		beanContextFactory.link(defaultMethodTransformer).to(IMethodTransformerExtensionExtendable.class).with(JsDefaultMethodTransformerName);
+
 		registerMethodTransformerExtension(beanContextFactory, JavaIoPrintstreamTransformer.class, java.io.PrintStream.class);
 		registerMethodTransformerExtension(beanContextFactory, JavaLangStringTransformer.class, java.lang.String.class);
 		registerMethodTransformerExtension(beanContextFactory, JavaLangCharSequenceTransformer.class, java.lang.CharSequence.class);
 	}
 
-	protected static IBeanConfiguration registerMethodTransformerExtension(IBeanContextFactory beanContextFactory,
+	private IBeanConfiguration registerMethodTransformerExtension(IBeanContextFactory beanContextFactory,
 			Class<? extends IMethodTransformerExtension> methodTransformerType, Class<?> type)
 	{
-		IBeanConfiguration methodTransformer = beanContextFactory.registerBean(methodTransformerType)//
-				.propertyRefs(EsmeraldaCoreModule.DefaultMethodTransformerName, EsmeraldaCoreModule.DefaultMethodParameterProcessor);
+		IBeanConfiguration methodTransformer = beanContextFactory.registerBean(methodTransformerType) //
+				.propertyRef(AbstractMethodTransformerExtension.defaultMethodTransformerExtensionProp, defaultMethodTransformer) //
+				.propertyRef(AbstractMethodTransformerExtension.defaultMethodParameterProcessorProp, defaultMethodParameterProcessor);
 		beanContextFactory.link(methodTransformer).to(IMethodTransformerExtensionExtendable.class).with(Lang.JS + type.getName());
 		return methodTransformer;
 	}
