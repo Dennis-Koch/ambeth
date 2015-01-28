a73545f0dfdbeaef7b875e8354d57d6983ed5302
Dennis Koch <dennis.koch@osthus.com>
2015-01-28 09:15:22 +0100

    jambeth-query: added IQueryBuilder.groupBy() support, added IQueryBuilder.select(IOperand) operand to select an aggregate function in addition to plain columns/properties

--- jambeth/jambeth-query/src/main/java/de/osthus/ambeth/query/IQueryBuilder.java
+++ jambeth/jambeth-query/src/main/java/de/osthus/ambeth/query/IQueryBuilder.java
@@ -125,7 +125,9 @@ public interface IQueryBuilder<T> extends IDisposable
 
 	IOperand function(String functionName, IOperand... operands);
 
-	IQueryBuilder<T> orderBy(IOperand column, OrderByType orderByType);
+	IQueryBuilder<T> groupBy(IOperand... operand);
+
+	IQueryBuilder<T> orderBy(IOperand operand, OrderByType orderByType);
 
 	/**
 	 * Please use selectProperty() instead
@@ -140,6 +142,8 @@ public interface IQueryBuilder<T> extends IDisposable
 	int selectColumn(String columnName, ISqlJoin join);
 
 	int selectProperty(String propertyName);
+
+	int select(IOperand operand);
 
 	ISqlJoin join(Class<?> entityType, IOperand columnBase, IOperand columnJoined, JoinType joinType);
 
--- jambeth/jambeth-test/src/test/java/de/osthus/ambeth/query/QueryEntity.java
+++ jambeth/jambeth-test/src/test/java/de/osthus/ambeth/query/QueryEntity.java
@@ -8,6 +8,10 @@ public class QueryEntity extends AbstractEntity
 {
 	public static final String Name1 = "Name1";
 
+	public static final String Fk = "Fk";
+
+	public static final String LinkTableEntity = "LinkTableEntity";
+
 	@Cascade(load = CascadeLoadMode.EAGER_VERSION)
 	protected JoinQueryEntity fk;
 
