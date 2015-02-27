19f3290b86142c13f2b246a5424d8f553d0bc1cc
andreas.menzel <andreas.menzel@osthus.com>
2015-02-27 17:32:23 +0100

    ambeth-doc:updated cmd file (update-configuration-extendable.cmd) to use the correct bundle mvn

--- doc/reference-manual/update-configuration-extendable.cmd
+++ doc/reference-manual/update-configuration-extendable.cmd
@@ -1,4 +1,10 @@
-@ECHO OFF
+@echo on
+cd ..\..\..\..
+
+call ".\SET_ENV.cmd"
+
+cd .\source\osthus-ambeth\doc\reference-manual
+
 @call mvn -f ../extendables-scanner clean compile
 @call mvn -f ../extendables-scanner exec:java -Dexec.mainClass="de.osthus.ambeth.extscanner.Main" "-Dexec.args=scan-path=""../reference-manual/target/data"" source-path=""../../ambeth;../../jambeth"" target-all-dir=""../reference-manual"" target-extendable-tex-dir=""${target-all-dir}/extendable"" target-properties-tex-dir=""${target-all-dir}/configuration"" target-feature-tex-dir=""${target-all-dir}/feature"" target-module-tex-dir=""${target-all-dir}/module"" target-annotation-tex-dir=""${target-all-dir}/annotation"" "
 @pause
