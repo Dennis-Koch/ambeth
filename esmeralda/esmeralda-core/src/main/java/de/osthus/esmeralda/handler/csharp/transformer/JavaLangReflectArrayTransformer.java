2cc5799d1b0fa0789e12ad9b1290c69cb43afe93
Dennis Koch <dennis.koch@osthus.com>
2015-03-07 15:15:03 +0100

    esmeralda: c#: added support for java.lang.Iterable, java.util.Enumeration, java.lang.reflect.Array, java.lang.reflect.Constructor uni: changed AbstractMethodTransformerExtension to support lookup for non-generic parameter keys if the key has no entry and contains generic parameter keys

---
+++ esmeralda/esmeralda-core/src/main/java/de/osthus/esmeralda/handler/csharp/transformer/JavaLangReflectArrayTransformer.java
@@ -0,0 +1,93 @@
+package de.osthus.esmeralda.handler.csharp.transformer;
+
+import com.sun.tools.javac.tree.JCTree.JCMethodInvocation;
+
+import de.osthus.ambeth.ioc.annotation.Autowired;
+import de.osthus.ambeth.log.ILogger;
+import de.osthus.ambeth.log.LogInstance;
+import de.osthus.esmeralda.handler.IMethodParameterProcessor;
+import de.osthus.esmeralda.handler.IOwnerWriter;
+import de.osthus.esmeralda.handler.ITransformedMethod;
+import de.osthus.esmeralda.handler.MethodKey;
+import de.osthus.esmeralda.handler.csharp.ICsHelper;
+import de.osthus.esmeralda.handler.uni.transformer.AbstractMethodTransformerExtension;
+import de.osthus.esmeralda.misc.IWriter;
+
+public class JavaLangReflectArrayTransformer extends AbstractMethodTransformerExtension
+{
+	@SuppressWarnings("unused")
+	@LogInstance
+	private ILogger log;
+
+	@Autowired
+	protected ICsHelper languageHelper;
+
+	@Override
+	public void afterPropertiesSet() throws Throwable
+	{
+		super.afterPropertiesSet();
+
+		mapTransformation(java.lang.reflect.Array.class, "newInstance", "System.Array", "CreateInstance", false, Class.class, int.class);
+
+		mapTransformation(java.lang.reflect.Array.class, "getLength", null, null, new IMethodParameterProcessor()
+		{
+			@Override
+			public void processMethodParameters(JCMethodInvocation methodInvocation, String owner, ITransformedMethod transformedMethod,
+					IOwnerWriter ownerWriter)
+			{
+				IWriter writer = context.getWriter();
+
+				writer.append("((");
+				languageHelper.writeTypeDirect("System.Array");
+				writer.append(')');
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(0));
+				writer.append(").Length");
+			}
+		}, Object.class).setStatic(true);
+
+		mapTransformation(java.lang.reflect.Array.class, "get", null, null, new IMethodParameterProcessor()
+		{
+			@Override
+			public void processMethodParameters(JCMethodInvocation methodInvocation, String owner, ITransformedMethod transformedMethod,
+					IOwnerWriter ownerWriter)
+			{
+				IWriter writer = context.getWriter();
+
+				writer.append("((");
+				languageHelper.writeTypeDirect("System.Array");
+				writer.append(')');
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(0));
+				writer.append(").Get(");
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(1));
+				writer.append(')');
+			}
+		}, Object.class, int.class).setStatic(true);
+
+		mapTransformation(java.lang.reflect.Array.class, "set", null, null, new IMethodParameterProcessor()
+		{
+			@Override
+			public void processMethodParameters(JCMethodInvocation methodInvocation, String owner, ITransformedMethod transformedMethod,
+					IOwnerWriter ownerWriter)
+			{
+				IWriter writer = context.getWriter();
+
+				writer.append("((");
+				languageHelper.writeTypeDirect("System.Array");
+				writer.append(')');
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(0));
+				writer.append(").Set(");
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(1));
+				writer.append(", ");
+				languageHelper.writeExpressionTree(methodInvocation.getArguments().get(2));
+				writer.append(')');
+			}
+		}, Object.class, int.class, Object.class).setStatic(true);
+	}
+
+	@Override
+	public ITransformedMethod buildMethodTransformation(MethodKey methodKey)
+	{
+		ITransformedMethod transformedMethod = super.buildMethodTransformation(methodKey);
+		return transformedMethod;
+	}
+}
