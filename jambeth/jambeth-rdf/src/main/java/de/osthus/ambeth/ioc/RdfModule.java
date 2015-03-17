22bb9dc9d1536d8c9d68ec4195d109c157484dae
Jochen Hormes <jochen.hormes@osthus.de>
2015-03-17 16:24:48 +0100

    jambeth-rdf: Correction in RdfModule.

--- jambeth/jambeth-rdf/src/main/java/de/osthus/ambeth/ioc/RdfModule.java
+++ jambeth/jambeth-rdf/src/main/java/de/osthus/ambeth/ioc/RdfModule.java
@@ -1,8 +1,11 @@
-package de.osthus.ambeth.rdf;
+package de.osthus.ambeth.ioc;
 
-import de.osthus.ambeth.ioc.IInitializingModule;
+import de.osthus.ambeth.ioc.annotation.FrameworkModule;
 import de.osthus.ambeth.ioc.factory.IBeanContextFactory;
+import de.osthus.ambeth.rdf.IRdfUtils;
+import de.osthus.ambeth.rdf.RdfUtils;
 
+@FrameworkModule
 public class RdfModule implements IInitializingModule
 {
 	@Override
