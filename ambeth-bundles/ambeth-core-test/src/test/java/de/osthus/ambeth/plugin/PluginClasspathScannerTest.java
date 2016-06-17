1dcd8f5c8dc56603972ccee6b3e8a05204842b61
shuang.bai <shuang.bai@osthus.com>
2016-06-17 20:22:06 +0200

    refactor the PluginClasspathScannerTest.java IOC way

--- ambeth-bundles/ambeth-core-test/src/test/java/de/osthus/ambeth/plugin/PluginClasspathScannerTest.java
+++ ambeth-bundles/ambeth-core-test/src/test/java/de/osthus/ambeth/plugin/PluginClasspathScannerTest.java
@@ -3,42 +3,36 @@ package de.osthus.ambeth.plugin;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.regex.Matcher;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import de.osthus.ambeth.Ambeth;
-import de.osthus.ambeth.bundle.Core;
 import de.osthus.ambeth.config.CoreConfigurationConstants;
 import de.osthus.ambeth.ioc.IServiceContext;
+import de.osthus.ambeth.ioc.annotation.Autowired;
 import de.osthus.ambeth.ioc.annotation.BootstrapModule;
 import de.osthus.ambeth.ioc.annotation.FrameworkModule;
-import de.osthus.ambeth.start.IAmbethApplication;
+import de.osthus.ambeth.testutil.AbstractIocTest;
+import de.osthus.ambeth.testutil.TestModule;
+import de.osthus.ambeth.testutil.TestProperties;
 
-public class PluginClasspathScannerTest
+@TestModule(PluginModule.class)
+@TestProperties(name = CoreConfigurationConstants.PluginPaths, value = "${pluginScanResource}/modules.jar;${pluginScanResource}/jars/;${pluginScanResource}/onejarfolder")
+public class PluginClasspathScannerTest extends AbstractIocTest
 {
-	IServiceContext applicationContext;
+	@Autowired
+	private IPluginClasspathScanner pluginScanner;
+	@Autowired
+	private IServiceContext applicationContext;
 
-	IPluginClasspathScanner pluginScanner;
-
-	@Before
-	public void before() throws URISyntaxException
+	@BeforeClass
+	public static void before() throws URISyntaxException
 	{
 		// get the scan resource absolute root path
-		String pluginScanResource = new File(this.getClass().getResource("/pluginScanResource").toURI()).getAbsolutePath();
-		// build the scan absolute paths
-		String value = "/modules.jar;/jars/;/onejarfolder".replaceAll("(^|;)", "$1" + Matcher.quoteReplacement(pluginScanResource));
-		value = value.replace('/', File.separatorChar);
-
-		// init ambeth factory
-		IAmbethApplication ambethApplication = Ambeth.createBundle(Core.class).withApplicationModules(PluginModule.class)
-				.withProperty(CoreConfigurationConstants.PluginPaths, value).start();
-
-		applicationContext = ambethApplication.getApplicationContext();
-		pluginScanner = applicationContext.getService(IPluginClasspathScanner.class);
-
+		String pluginScanResource = new File(PluginClasspathScannerTest.class.getResource("/pluginScanResource").toURI()).getAbsolutePath();
+		// for build the scan absolute paths
+		System.setProperty("pluginScanResource", pluginScanResource);
 	}
 
 	@Test
