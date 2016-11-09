e11f92203e95aa18349f7bdca9d88686bc8c1f43
daniel.mueller <daniel.mueller@osthus.com>
2016-11-09 14:45:21 +0100

    add another test

--- jambeth/jambeth-test/src/test/java/de/osthus/ambeth/shell/ErrorModeTest.java
+++ jambeth/jambeth-test/src/test/java/de/osthus/ambeth/shell/ErrorModeTest.java
@@ -1,5 +1,6 @@
 package de.osthus.ambeth.shell;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import de.osthus.ambeth.ioc.IInitializingModule;
@@ -7,6 +8,7 @@ import de.osthus.ambeth.ioc.annotation.Autowired;
 import de.osthus.ambeth.ioc.factory.IBeanContextFactory;
 import de.osthus.ambeth.shell.ErrorModeTest.ErrorModeTestModule;
 import de.osthus.ambeth.shell.core.ShellContext.ErrorMode;
+import de.osthus.ambeth.shell.core.resulttype.SingleResult;
 import de.osthus.ambeth.shell.ioc.AmbethShellModule;
 import de.osthus.ambeth.testutil.AbstractIocTest;
 import de.osthus.ambeth.testutil.TestModule;
@@ -34,6 +36,25 @@ public class ErrorModeTest extends AbstractIocTest
 		shell.executeCommand("set", "error.mode=" + ErrorMode.THROW_EXCPETION);
 		shell.executeCommand("echo", "error mode is '$error.mode'");
 		shell.executeCommand("throw", "message=ohhhh look, an exception!");
+	}
+
+	@Test
+	public void testErrorMode_THROW_EXCEPTION_AND_CONTINUE()
+	{
+		try
+		{
+			shell.executeCommand("set", "error.mode=" + ErrorMode.THROW_EXCPETION);
+			shell.executeCommand("throw", "message=ohhhh look, another exception!");
+		}
+		catch (Exception e)
+		{
+
+		}
+
+		String value = "foo";
+		SingleResult result = (SingleResult) shell.executeCommand("echo", value);
+		result.getValue();
+		Assert.assertEquals(result.getValue(), value);
 	}
 
 	@Test
