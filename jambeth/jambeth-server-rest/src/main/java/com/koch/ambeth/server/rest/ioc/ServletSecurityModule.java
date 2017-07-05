744115623b4419ed150f3a705868c1e540efddf8
Dennis Koch <dennis.koch@bruker.com>
2017-07-05 08:43:58 +0200

    jambeth-server-rest: simplified logic in ServletAuthorizationChangeListener

--- jambeth/jambeth-server-rest/src/main/java/com/koch/ambeth/server/rest/ioc/ServletSecurityModule.java
+++ jambeth/jambeth-server-rest/src/main/java/com/koch/ambeth/server/rest/ioc/ServletSecurityModule.java
@@ -23,20 +23,14 @@ limitations under the License.
 import com.koch.ambeth.ioc.IInitializingModule;
 import com.koch.ambeth.ioc.config.IBeanConfiguration;
 import com.koch.ambeth.ioc.factory.IBeanContextFactory;
-import com.koch.ambeth.log.ILogger;
-import com.koch.ambeth.log.LogInstance;
 import com.koch.ambeth.security.IAuthorizationChangeListenerExtendable;
 import com.koch.ambeth.server.rest.ServletAuthorizationChangeListener;
 
 public class ServletSecurityModule implements IInitializingModule {
-	@SuppressWarnings("unused")
-	@LogInstance
-	private ILogger log;
-
 	@Override
 	public void afterPropertiesSet(IBeanContextFactory beanContextFactory) throws Throwable {
-		IBeanConfiguration bean =
-				beanContextFactory.registerBean(ServletAuthorizationChangeListener.class);
+		IBeanConfiguration bean = beanContextFactory
+				.registerBean(ServletAuthorizationChangeListener.class);
 		beanContextFactory.link(bean).to(IAuthorizationChangeListenerExtendable.class).optional();
 	}
 }
