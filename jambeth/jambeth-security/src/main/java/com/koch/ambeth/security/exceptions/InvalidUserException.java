10c666c5d15de9353cc1418f758a5634f2a792b5
Dennis Koch <dennis.koch@bruker.com>
2017-10-17 00:33:26 +0200

    jambeth-security-server: fixed issue in AuthorizationProcess throwing a PasswordChangeRequiredException for an inactive user account

--- jambeth/jambeth-security/src/main/java/com/koch/ambeth/security/exceptions/InvalidUserException.java
+++ jambeth/jambeth-security/src/main/java/com/koch/ambeth/security/exceptions/InvalidUserException.java
@@ -23,14 +23,7 @@ limitations under the License.
 public class InvalidUserException extends SecurityException {
 	private static final long serialVersionUID = -1163535828252169857L;
 
-	private final String sid;
-
-	public InvalidUserException(String sid) {
-		super("User '" + sid + "' is not a valid user.");
-		this.sid = sid;
-	}
-
-	public String getSID() {
-		return sid;
+	public InvalidUserException(String message) {
+		super(message);
 	}
 }
